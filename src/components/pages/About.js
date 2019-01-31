702138b7b0879f25f73d51a7b877e8f5ea1db191
Jinwoo <kingjinwk@gmail.com>
2019-01-30 18:38:56 -0800

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app v1.0.0. This is my first React app ever, and built off a YouTube tutorial</p>
+    </React.Fragment>
+  )
+}
+export default About;
